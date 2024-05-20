68453f5290793c154004b2289a45f037c272eac8
joshuapostol <156434541+joshuapostol@users.noreply.github.com>
2024-05-21 07:45:35 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+total_requests = 100000000  # Total number of requests
+requests_per_second = 1000000  # Number of requests per second
+num_ip_addresses = 10  # Number of IP addresses to use
+target_url = "https://example.com"  # Target URL
