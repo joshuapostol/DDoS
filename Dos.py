c6fbe520ca43eb6590c3faa145b8b17ca6ad7d50
Churchill <141407524+Churchillabing1@users.noreply.github.com>
2024-05-20 23:24:49 +0800

    Update Dos.py

--- Dos.py
+++ Dos.py
@@ -10,7 +10,7 @@ print("""
    ░       ░        ░ ░        ░  
  ░       ░                          
 \033[0m
-\033[31mCreated by Joshua Apostol\033[0m
+\033[31mCreated by tete\033[0m
 \033[31mPlease do not attack gov or edu website thankyou!!!\033[0m
 """)
 
